6e0e0f40dff9b2770807c3251659e474d7da6c6e
intel <adityakhaladkar98@gmal.com>
2023-02-18 16:15:33 +0530

    added signin page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,63 @@
 <template>
   <div>
-    <h1>Nirvana</h1>
+    <div class="navbar">
+    <div class="navbar-left">
+      <router-link to="/" class="navbar-brand">
+        Nirvana
+      </router-link>
+    </div>
+    <div class="navbar-right">
+      <router-link to="/signin" class="navbar-link">
+        Sign in
+      </router-link>
+      <router-link to="/signup" class="navbar-link">
+        Sign up
+      </router-link>
+    </div>
+  </div>
   </div>
 </template>
 
 <script>
   export default {
-    
+    name: 'Navbar'
   }
 </script>
 
 <style lang="scss" scoped>
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+  background-color: white;
+  color: #fff;
+}
 
+.navbar-left {
+  display: flex;
+  align-items: center;
+}
+
+.navbar-brand {
+  font-size: 24px;
+  font-weight: bold;
+  text-decoration: none;
+  color: #000;
+}
+
+.navbar-right {
+  display: flex;
+}
+
+.navbar-link {
+  margin-left: 20px;
+  font-size: 18px;
+  text-decoration: none;
+  color: #000;
+}
+
+.navbar-link:hover {
+  text-decoration: underline;
+}
 </style>
---
+++ src/views/SignIn.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="login-container">
+      <form>
+        <h2>Login</h2>
+        <div class="form-group">
+          <label for="username">Username:</label>
+          <input type="text" id="username" v-model="username" required />
+        </div>
+        <div class="form-group">
+          <label for="password">Password:</label>
+          <input type="password" id="password" v-model="password" required />
+        </div>
+        <button type="submit" @click.prevent="submitForm">Submit</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        username: '',
+        password: ''
+      };
+    },
+    methods: {
+      submitForm() {
+        // Implement the logic to submit the form
+        console.log(`Username: ${this.username} Password: ${this.password}`);
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .login-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+  
+  form {
+    width: 400px;
+    background-color: white;
+    padding: 40px;
+    border-radius: 10px;
+    box-shadow: 0 0 10px lightgray;
+    text-align: center;
+  }
+  
+  h2 {
+    margin-bottom: 40px;
+  }
+  
+  .form-group {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 20px;
+  }
+  
+  label {
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
+  
+  input {
+    padding: 10px;
+    border: 1px solid lightgray;
+    border-radius: 5px;
+    font-size: 14px;
+  }
+  
+  button[type="submit"] {
+    background-color: dodgerblue;
+    color: white;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    font-size: 14px;
+    cursor: pointer;
+  }
+  </style>  
