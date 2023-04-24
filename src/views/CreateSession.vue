8e1723b01df97611f3d679dc8f1e1f27d67262f5
intel <adityakhaladkar98@gmal.com>
2023-04-24 09:39:35 +0530

    create session page added

--- src/views/CreateSession.vue
+++ src/views/CreateSession.vue
@@ -17,52 +17,17 @@
     </v-app-bar>
 
     <v-container fluid class="container">
-      <v-col class="add-session-form">
-        <h2>Please fill in the following details</h2>
-
-        <v-card height="400" class="card">
-          <v-file-input
-            v-model="file"
-            label="Select an image"
-            accept="image/*"
-            @change="onFileChange"
-          ></v-file-input>
-        </v-card>
-
-      </v-col>
+     
     </v-container>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      file: null,
-      imageUrl: null,
-    };
-  },
-  methods: {
-    onFileChange() {
-      const reader = new FileReader();
-
-      reader.onload = (event) => {
-        this.imageUrl = event.target.result;
-      };
-
-      reader.readAsDataURL(this.file);
-    },
-  },
+  
 };
 </script>
 
 <style lang="scss" scoped>
-.add-session-form {
-  display: block;
-  text-align: center;
-  height: 100%;
-}
-.card {
-    margin: 3rem;
-}
+
 </style>
