10aa8c2b3b44d99a0b8e2b28e458adcc1608fc01
aryan-narayan09 <125352530+aryan-narayan09@users.noreply.github.com>
2023-04-27 02:19:15 -0700

    Add top app bar
    Add app bar component for easy navigation. Contains logo, login and sign up buttons which redirect to respective pages.

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,6 +1,31 @@
 <template>
   <v-container fluid class="container">
     
+    <v-app-bar
+      app
+      color="secondary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Nirvana logo"
+          class="shrink mr-2"
+          contain
+          src="@/assets/nirvanalogo.png"
+          transition="scale-transition"
+          width="30"
+        />
+
+        <h2 >Nirvana</h2>
+
+        
+      </div>
+      <v-spacer></v-spacer>
+
+        <v-btn text @click="$router.push('/login')">Login</v-btn>
+        <v-btn text @click="$router.push('/signUp')">Sign Up</v-btn>
+
+    </v-app-bar>
     
     <v-row justify="center" align="center">
       <v-col cols="4" align="center" offset="1">
